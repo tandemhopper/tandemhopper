865c99a2c10411325331474c58295856c685d3d9
tandemhopper <maximilian.dahlke@web.de>
2026-08-29 21:01:53 +0200

    Redirect www domain to tandemhopper.de

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  trailingSlash: false
+  trailingSlash: false,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.tandemhopper.de'
+          }
+        ],
+        destination: 'https://tandemhopper.de/:path*',
+        permanent: true
+      }
+    ];
+  }
 };
 export default nextConfig;
